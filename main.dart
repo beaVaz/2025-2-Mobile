a025d06f5b116ab9d56a7f5e865d9d9576f5a18c
beaVaz <140008389+beaVaz@users.noreply.github.com>
2025-09-04 00:39:40 +0000

    01-exercicio

--- main.dart
+++ main.dart
@@ -1,9 +1,18 @@
 import 'dart:io';
 void main (){
-int idade = 23;
-if(idade >= 18){
-    print('pode entrar na balada');
-}
-print('fim do programa');
+int renda = 5000;
+int saldo = 100000;
 
+if (renda >= 60000 && saldo >= 100000) {
+    print('Exclusive');
+  } else if (renda >= 30000) {
+    print('Select');
+  } else if (renda >= 5000) {
+    print('Vangogh');
+  } else if (renda >= 1400) {
+    print('Varejo');
+  } else {
+    print('No money bb');
+  }
 }
+
